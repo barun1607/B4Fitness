be8d07a864e220e29c0ed55cf91bb618939784d9
barun1607 <varunp2000@gmail.com>
2020-11-23 23:56:01 +0530

    Added some images

--- lib/widgets/NavButton.dart
+++ lib/widgets/NavButton.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class NavButton extends StatelessWidget {
-  final Color _color = Colors.red;
-
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 150,
       width: 150,
-      child: FlatButton(
+      child: RaisedButton(
         onPressed: () {},
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
@@ -16,12 +15,23 @@ class NavButton extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text("Hello"),
+            SizedBox(
+              child: Image.asset("assets/images/1200px-Calendar_font_awesome.svg.png"),
+              height: 100,
+              width: 100,
+            ),
+            Text(
+              "Hello",
+              style: TextStyle(
+                fontFamily: "Montserrat",
+                fontSize: 26,
+              ),
+            ),
           ],
         ),
         color: Colors.red,
+        elevation: 5,
         padding: EdgeInsets.all(0),
-        highlightColor: Colors.red[600],
       ),
     );
   }
